1d2f9bd7b112705e102a913c222781ba1868a9d9
SkyBeastMC <skybeastmc@gmail.com>
2018-03-06 13:32:23 +0100

    Error handling for listeners

--- src/decorators/on.js
+++ src/decorators/on.js
@@ -1,13 +1,22 @@
 import { INSTANCE } from '../Modules';
 import { client } from '../discord';
+import { red, white } from 'chalk';
 
 export const listeners = new Map();
 
 export default function on(event) {
 	return (target, key, descriptor) => {
 		if (!listeners.has(event)) listeners.set(event, []);
-		const listener = (...args) =>
-			descriptor.value.apply(target[INSTANCE], args);
+		const listener = (...args) => {
+			try {
+				descriptor.value.apply(target[INSTANCE], args);
+			} catch (e) {
+				console.log(red.bold('Something unexpected happened when dispatching event "' + white.bgRed(event) + '" to listener "'
+					+ white.bgRed(`${target.constructor.name}.${key}`) + '"!'));
+				console.log(red.bold('Stacktrace: ') + red(e ? e.stack : 'Error ' + e));
+				console.log(red.italic.bold('Please fix me senpai'));
+			}
+		}
 		listeners.get(event).push(listener);
 		client.on(event, listener);
 		return descriptor;
