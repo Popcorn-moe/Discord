d8efbb94027ea5d78b06809b8005562509881374
SkyBeastMC <skybeastmc@gmail.com>
2018-03-19 00:20:28 +0100

    Format

--- src/modules/Music/ListenMoeStreamer.js
+++ src/modules/Music/ListenMoeStreamer.js
@@ -36,34 +36,29 @@ export default class ListenMoeStreamer extends EventEmitter {
 	}
 
 	get embed() {
-		if (this.infos)
-			return Promise.resolve(
-				new RichEmbed()
-					.setTitle(`${this.infos.artist_name} - ${this.infos.song_name}`)
-					.setImage('https://listen.moe/files/images/kanna.gif')
-					.setFooter(
-						`Requested by ${this.infos.requested_by || 'anonymous'} - ${
-							this.infos.listeners
-						} auditeurs`
-					)
-					.setColor(0xec1a55)
-			);
-		else
-			return Promise.resolve(
-				new RichEmbed()
-					.setTitle('Listen.moe')
-					.setImage('http://listen.moe/files/images/fb_share.jpg')
-					.setColor(0xec1a55)
-			);
+		return Promise.resolve(
+			this.infos
+				? new RichEmbed()
+						.setTitle(`${this.infos.artist_name} - ${this.infos.song_name}`)
+						.setImage('https://listen.moe/files/images/kanna.gif')
+						.setFooter(
+							`Requested by ${this.infos.requested_by || 'anonymous'} - ${
+								this.infos.listeners
+							} auditeurs`
+						)
+						.setColor(0xec1a55)
+				: new RichEmbed()
+						.setTitle('Listen.moe')
+						.setImage('http://listen.moe/files/images/fb_share.jpg')
+						.setColor(0xec1a55)
+		);
 	}
 
 	get title() {
-		if (this.infos) {
-			return Promise.resolve(
-				`${this.infos.artist_name} - ${this.infos.song_name}`
-			);
-		} else {
-			return Promise.resolve('Listen.moe');
-		}
+		return Promise.resolve(
+			this.infos
+				? `${this.infos.artist_name} - ${this.infos.song_name}`
+				: 'Listen.moe'
+		);
 	}
 }
