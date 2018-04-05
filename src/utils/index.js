fe5a4afb5a92006ed940559f5e8210046be0d555
loucass003 <loucass003@gmail.com>
2018-04-05 21:39:28 +0200

    Fix import issue

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,6 @@ import * as embeds from './embeds';
 import * as members from './members';
 import { error, warn, errHandle, errorDiscord } from './log';
 import load from './load';
+import random from './random'
 
-export { embeds, members, error, warn, errHandle, errorDiscord, load };
+export { embeds, members, error, warn, errHandle, errorDiscord, load, random };
